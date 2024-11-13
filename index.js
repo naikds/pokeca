6ce570e00c135a29f1d7ee5c33c00090c38171ea
naikds <yistmmy1284@gmail.com>
2024-11-13 22:29:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
         draggable.addEventListener('dragstart', (e) => {
             e.dataTransfer.setData('text/plain',e.target.id);
         });
+
+        draggable.addEventListener('touchstart',(e)=>{
+            e.dataTransfer.setData('text/plain',e.target.id);
+        })
     });
 
     dropzones.forEach(dropzone => {
         dropzone.addEventListener('dragover',(e) =>{
             e.preventDefault();
         })
+        dropzone.addEventListener('touchmove',(e)=>{
+            e.preventDefault();
+        })
         dropzone.addEventListener('drop',(e)=>{
+            e.preventDefault();
+            const id = e.dataTransfer.getData('text/plain');
+            const item = document.getElementById(id);
+
+            dropzone.appendChild(item);
+        })
+        dropzone.addEventListener('touchend',(e)=>{
             e.preventDefault();
             const id = e.dataTransfer.getData('text/plain');
             const item = document.getElementById(id);
